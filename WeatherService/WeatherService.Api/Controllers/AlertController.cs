daddc88b383b6d8beb933d3912fd7df3581c3f17
leonard <leonakalun2@gmail.com>
2026-03-24 22:32:56 +0800

    fix bug

--- WeatherService/WeatherService.Api/Controllers/AlertController.cs
+++ WeatherService/WeatherService.Api/Controllers/AlertController.cs
@@ -28,7 +28,7 @@ namespace WeatherService.Api.Controllers
         /// </summary>
         /// <param name="req"></param>
         /// <returns></returns>
-        [HttpPost]
+        [HttpPost("CreateAlert")]
         [ProducesResponseType(typeof(BaseApiResponseDto<AlertResponseDto>), 201)]
         [ProducesResponseType(typeof(BaseApiResponseDto<AlertResponseDto>), 400)]
         public async Task<IActionResult> Create([FromBody] CreateAlertRequestDto req)
@@ -50,7 +50,7 @@ namespace WeatherService.Api.Controllers
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
-        [HttpGet("{id:guid}")]
+        [HttpGet("GetAlertById")]
         [ProducesResponseType(typeof(BaseApiResponseDto<AlertResponseDto>), 200)]
         [ProducesResponseType(typeof(BaseApiResponseDto<AlertResponseDto>), 404)]
         public async Task<IActionResult> GetById(Guid id)
@@ -66,7 +66,7 @@ namespace WeatherService.Api.Controllers
         /// </summary>
         /// <param name="email"></param>
         /// <returns></returns>
-        [HttpGet]
+        [HttpGet("GetAlertByEmail")]
         [ProducesResponseType(typeof(BaseApiResponseDto<IList<AlertResponseDto>>), 200)]
         public async Task<IActionResult> GetByEmail([FromQuery] string email)
         {
@@ -84,7 +84,7 @@ namespace WeatherService.Api.Controllers
         /// <param name="id"></param>
         /// <param name="req"></param>
         /// <returns></returns>
-        [HttpPatch("{id:guid}")]
+        [HttpPatch("UpdateAlertContent")]
         [ProducesResponseType(typeof(BaseApiResponseDto<AlertResponseDto>), 200)]
         [ProducesResponseType(typeof(BaseApiResponseDto<AlertResponseDto>), 404)]
         public async Task<IActionResult> Update(Guid id, [FromBody] UpdateAlertRequestDto req)
@@ -105,7 +105,7 @@ namespace WeatherService.Api.Controllers
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
-        [HttpDelete("{id:guid}")]
+        [HttpDelete("DeleteAlert")]
         [ProducesResponseType(typeof(BaseApiResponseDto<object>), 200)]
         [ProducesResponseType(typeof(BaseApiResponseDto<object>), 404)]
         public async Task<IActionResult> Delete(Guid id)
@@ -122,7 +122,7 @@ namespace WeatherService.Api.Controllers
         /// Manually trigger alert (for testing)
         /// </summary>
         /// <returns></returns>
-        [HttpPost("process")]
+        [HttpPost("ProcessAlert")]
         [ProducesResponseType(typeof(BaseApiResponseDto<object>), 200)]
         public async Task<IActionResult> Process()
         {
